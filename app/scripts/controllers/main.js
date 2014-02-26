57867c7ce374479a6bd4ce0bdc280bea9fe98af1
Wouter Deman <wouter.deman@telenet.be>
2014-02-26 21:02:55 +0100

    Added total tracked time & small rename's

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,32 @@
 'use strict';
 
 angular.module('timesheetdashboardApp')
-	.controller('MainCtrl', function($scope, $http) {
+	.controller('MainCtrl', function ($scope, $http) {
 		$scope.awesomeThings = [
 			'HTML5 Boilerplate',
 			'AngularJS',
 			'Karma'
 		];
 
-		$http.get('http://timesheetservice.herokuapp.com/dashboard/lastuserlocation').success(function(data) {
+		$http.get('http://timesheetservice.herokuapp.com/dashboard/lastuserlocation').success(function (data) {
 			$scope.userlocations = data;
 		});
 
-		$http.get('http://timesheetservice.herokuapp.com/dashboard/last10').success(function(data) {
+		$http.get('http://timesheetservice.herokuapp.com/dashboard/last10').success(function (data) {
 			$scope.last10 = data;
 		});
 
-		$http.get('http://timesheetservice.herokuapp.com/dashboard/count').success(function(data) {
+		$http.get('http://timesheetservice.herokuapp.com/dashboard/count').success(function (data) {
 			$scope.count = data;
+		});
+
+		$http.get('http://timesheetservice.herokuapp.com/dashboard/trackedminutes').success(function (minutes) {
+			var seconds = minutes * 60;
+			var numdays = Math.floor(seconds / 86400);
+			var numhours = Math.floor((seconds % 86400) / 3600);
+			var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
+			$scope.days = numdays;
+			$scope.hours = numhours;
+			$scope.minutes = numminutes;
 		});
 	});
